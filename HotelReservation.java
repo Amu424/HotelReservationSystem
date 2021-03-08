b05fcfd0e746986ee837e3a3dbe171887812ee56
softtech <amols424@gmail.com>
2021-03-08 09:28:48 +0530

    Added hotel in it

--- HotelReservation.java
+++ HotelReservation.java
@@ -1,10 +1,18 @@
 package hotelReservation;
 
 public class HotelReservation {
+	String HotelName;
+	int rateForRegularCustomer;
+	public HotelReservation(String HotelName, int rateForRegularCustomer) {
+		this.HotelName=HotelName;
+		this.rateForRegularCustomer=rateForRegularCustomer;
+	}
+
+	
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to hotel reservation System");
-
+		HotelReservation hr=new HotelReservation("Lakewood",110);
 	}
 
 }
